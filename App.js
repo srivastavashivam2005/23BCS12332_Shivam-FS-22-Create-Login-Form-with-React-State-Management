04aa678eda96ee95756a17e8c10d23931574d1fb
srivastavashivam2005 <coolestbadboy2005@gmail.com>
2025-11-06 11:22:05 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from "./LoginForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>React Login Form</h1>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
